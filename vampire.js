5290f8ad98119126165925d23c8b5d24cc1f4457
KamyllaAlmeida <kamylla.florencio@gmail.com>
2018-11-23 07:30:24 +0000

    Intro to Trees: Vampr! Assignment done

--- vampire.js
+++ vampire.js
@@ -10,22 +10,33 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
-
+     let creator = this.creator;
+    var numberOfVampires = 0;
+    while(creator != null) {
+      creator = creator.creator; 
+      numberOfVampires++;
+    }
+    return numberOfVampires;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
+    let numberThisFromOriginal = this.numberOfVampiresFromOriginal;
+    let numberVampireFromOriginal = vampire.numberOfVampiresFromOriginal;
 
+    return (numberVampireFromOriginal > numberThisFromOriginal);
+  
   }
 
   /** Stretch **/
@@ -36,9 +47,26 @@ class Vampire {
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
+    let currentVampire = this;
+    let aVampire = vampire;
+    
+    if(currentVampire === aVampire) {
+      return currentVampire;
+    }
 
+    if(currentVampire.numberOfVampiresFromOriginal < aVampire.numberOfVampiresFromOriginal) {
+      aVampire = aVampire.creator;
+      return currentVampire.closestCommonAncestor(aVampire);
+    } else if(currentVampire.numberOfVampiresFromOriginal > aVampire.numberOfVampiresFromOriginal) {
+      currentVampire = currentVampire.creator;
+      return currentVampire.closestCommonAncestor(aVampire);
+    }
+    if(currentVampire.numberOfVampiresFromOriginal === aVampire.numberOfVampiresFromOriginal) {
+      return currentVampire.creator.closestCommonAncestor(aVampire.creator);
+    }
+    
   }
-}
+  
 
 module.exports = Vampire;
 
